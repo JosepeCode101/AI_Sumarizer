b069e29d1e610e88a3c58b16acd56cd2514109ba
JosepeCode <72314772+JosepeCode101@users.noreply.github.com>
2023-05-15 14:57:30 +0200

    hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { logo } from '../assets';
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+
+        <nav className='flex justify-between items-center w-full mb-10 pt-3'>
+            <img src={logo} alt='sumz_logo' className='w-28 object-contain'/>
+
+            <button 
+                type="button" 
+                onClick={() => window.open('https://github.com/JosepeCode101/AI_Sumarizer')}
+                className='black_btn'
+            >
+            GitHub
+            </button>
+
+        </nav>
+
+        <h1 className='head_text'>
+            Summarize articles with <br className='max-md:hidden'/>
+            <span className='orange_gradient'>OpenAI GPT-4</span>
+        </h1>
+        <h2 className='desc'>
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+        </h2>
+
+    </header>
   )
 }
 
